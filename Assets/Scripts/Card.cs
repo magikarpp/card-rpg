a71951481c0bab36476d534f0314d6db24ee7de0
magikarp <shimjay1@gmail.com>
2022-06-18 02:06:50 -0400

    prefab for card body

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+[System.Serializable]
+public class Card
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string cardName;
+    [TextArea(1,3)]
+    public string cardDescription;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Sprite cardSprite;
+    public Sprite cardBgSprite;
+
+    public int cardMana;
+    public int cardStamina;
+    public int cardCooldown;
 }
